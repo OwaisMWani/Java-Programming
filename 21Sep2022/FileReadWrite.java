d7da42a0da171115cd9b35a27bfedf51e0a62c84
Owais Muhammad Wani <113293504+OwaisMWani@users.noreply.github.com>
2022-09-21 18:37:27 +0530

    Update and rename FileReader.java to FileReadWrite.java

--- 21Sep2022/FileReadWrite.java
+++ 21Sep2022/FileReadWrite.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.util.Scanner;
 import java.io.IOException;
-public class FileReader {
+public class FileReadWrite {
 	public static void main(String[] args) throws IOException{
 		Reader read=new Reader();
 		Writer write=new Writer();
