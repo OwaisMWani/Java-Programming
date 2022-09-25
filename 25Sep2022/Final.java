6f93b5f974a0973b5bd3ab2ed50bfdab5cb4bf90
OwaisMWani <owaismohd19@gmail.com>
2022-09-25 17:27:56 +0530

    Learning

---
+++ 25Sep2022/Final.java
@@ -0,0 +1,10 @@
+public class Final {
+	final int A = 35;
+	final int MAX = 55;
+	final int MIN = 0; //a final method or variable/string cannot be changed anywhere and they remain constant
+	static final double PI = 3.14159;
+	public static void main(String[] args) {
+		Final f=new Final();
+		System.out.println(f.MAX);
+	}
+}
